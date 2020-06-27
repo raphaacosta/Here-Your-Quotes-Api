0c37b6ba8403db4384fb3ae31ac1f09a28fe2d89
Raphael <raphael@iMac-de-Raphael.local>
2020-06-26 21:42:23 -0300

    added knexfile to run the db

---
+++ knexfile.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'rootroot',
+    database: 'hereyourquotes',
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+  },
+}
