94685fc3d9c895b4aaf9c00ee032ad4b28236958
Raphael <raphael@iMac-de-Raphael.local>
2020-07-03 17:58:27 -0300

    creating all routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,25 @@
 import express from 'express';
 
+import UsersController from './controllers/usersController';
+import QuotesController from './controllers/quotesController';
+import SessionController from './controllers/sessionController';
+import ProfileController from './controllers/profileController'
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'hello Here Your Quotes'});
-});
+const usersController = new UsersController;
+const quotesController = new QuotesController;
+const profileController = new ProfileController;
+const sessionController = new SessionController;
+
+routes.post('/sessions', sessionController.create);
+routes.get('/profile', profileController.index);
+
+routes.post('/users_create', usersController.create);
+routes.post('/login', usersController.authentication);
+
+routes.get('/quotes', quotesController.index);
+routes.post('/quotes', quotesController.create);
+routes.delete('/quotes/:id', quotesController.destroy);
 
 export default routes;
