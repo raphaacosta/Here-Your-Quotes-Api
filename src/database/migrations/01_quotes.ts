d476ffac5668cd432429eb7570e486b6903155a6
Raphael <raphael@iMac-de-Raphael.local>
2020-07-03 18:01:32 -0300

    abstracting http methods

--- src/database/migrations/01_quotes.ts
+++ src/database/migrations/01_quotes.ts
@@ -4,8 +4,15 @@ export async function up(knex: Knex){
   return knex.schema.createTable('quotes', table => {
     table.increments('id').primary();
     table.string('content').notNullable();
-    table.string('author').notNullable();
-    table.string('source').notNullable();
+    table.string('author').notNullable().defaultTo('Unknown');
+    table.string('complement');
+    table.integer('user_id')
+      .notNullable()
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
   });
 }
 
